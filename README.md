ff04befd1dd98e24c2ab6ffbf5ba1efd72055ee8
rafael-santiago <voidbrainvoid@gmail.com>
2015-05-19 23:57:15 -0300

    Init

---
+++ README.md
@@ -0,0 +1,37 @@
+# shh
+
+This code is an old ioccc entry of mine. Unfortunately I have never won with this.
+Maybe this code does not implement a rather useless task...
+
+Anyway, this code implements a vocal reductor. It works with ``WAVE`` format only and
+the audio must be encoded as ``PCM`` (no compression, raw sound).
+
+## How to build it?
+
+Originally I had used ``Make`` but it sucks in several ways.
+
+Now in order to build it you need [Hefesto](https://github.com/rafael-santiago/hefesto.git).
+
+After install ``Hefesto`` on your system being inside ``shh src`` subdirectory just type
+the following command on ``shell``:
+
+        root@zephyr:~/shh/src# hefesto
+
+A subdirectory ``bin`` will be created and inside it you probably will find an ``elf``
+named ``shh``.
+
+I think now with ``Hefesto`` here you can build ``shh`` on Windows. If it worked for you,
+please let me know.
+
+## How to use shh?
+
+The usage of ``shh`` is pretty straightforward:
+
+``shh <.wav input file path> <.wav output file path>``
+
+Note that is important avoid using instrumental songs ``shh``, otherwise the vocal reduction
+will not work... ok?! :-P
+
+and...
+
+# Please do not promote karaoke parties... I beg you!!
---
+++ src/shh.c
@@ -0,0 +1,47 @@
+/*one-two-three-four...*\olensnryfnagvntbzprqvgebpxf)i?z;*/#include "bazzinga.h"
+#define jh(i)((((zy)(i))==0x0)?hh(&c,qp[1&(~(~0xf) )]):((zy)(i)==1)?kk(qp[(2>>1\
+)&(~0x0)],c.z.j):(( (zy)(i))==(0x4>>1))?lz(L,c.z.j,X):(((zy)(i))==(0x7>>1))?zl(\
+&c,X,R,qp[2],c.z.j):( exit(1),K("unk %d",i),0))/*|06x0({ = ]11[L  ab;}"00x\a8x*/
+ab*WTF={"\x66\x61\x63\x74\x04\x00\x00\x00\xc4\xc5\x8a\x00"}; ba  L[11] = {(0x60|
+0x03),(0x6<<((1<<1)<<1)) |((0x6>>3)|0x1<<3),0x10>>4|(0x06<<4),(((0x6<<1)<<2)<<1)
++0xe,14+((0x03<<1)<<4), ((((0x60>>4)<<2)<<1)<<1)|0x5,(((0xc0>>1)>>2)>>1)+(0x60),
+32|14,(0x6<<4)+ (24>>1),(ba)((0xff<<8)&~(0xff))};ba R[11]={((0x30>>(8>>1))|(0x06
+<<(0x2<<1))),((3<<1)<<4)+(0x10>>1),0x60|(((0x10>>1)>>2)>>1),(28>>1)+((3<<1)<<4),
+14+(6<<4),(0x6<<4)+5,(0x6<<4)+(((0xc0)>>2)>>2),((0x1<<1)<<4)+0xe,(((0x3<<1)|0x1)
+<<4)|2,(ba)(((~(0x00))<<8)&~(~0x00))};N M k {zy a;wx s;wx d;zy f;zy g;wx h;wx j;
+}l;N M Y {l z;zy x;}W;A hh(W*c,ab*f){E*fwv;zy  h;(!(fwv=S(f,"rb")))?K(b,f):((C(&
+h,4,1,fwv),((h!=zi)?(K(bI,f),I(fwv),h=0):(fseek(fwv,8L,J),C(&h,4,1,fwv),(h!=iz)?
+(K(bII,f),I(fwv),h=0):(C(&h,4,1,fwv),(h!=zz)?(K( bV , f),I(fwv),h=0) :(    fseek
+(fwv,36L,J),C(&h,4,1,fwv),(h!=hj)?(K(Z,f),I(fwv),h=0):(fseek(fwv,16L,     J),C(&
+c->z,20, 1 ,fwv),          fseek(  fwv,40,J),C (     &c->x,4,1,fwv),I(    fwv),h
+=!0)))))));G      (h);}A   kk(     ab*x,yz j) {E     *v,*___, *____ ;     ba __[
+O /8 ]; yz _,     _____    ;(!     (v = S(x,"rb"    ))? (K (ZI , x),     _____=0
+):((!( ___ =     S(F L ,   "wb"    )))?(K(ZII,L)    ,I(v),_____=0):(     (!(____
+=S(F R,"wb")      ))?(  K(ZII,    R),I(v),_____     =0     ):(_=j/8,    fseek(v,
+44,J),_____        =1  ) ) ) )    ;       if(!              _____)G    ( ~ ( ~ (
+_____  ) ) );        while                 (!     feof(     v)) {C(    __,_,1,v)
+;_____  =  _____       & 1 ;        (      !      _____     )?(D(__    ,_,1,___)
+):(D(__,_,1,____)        ) ;     _____     ++;    }I(v)    ;I(___);    I(____);G
+(1);}A lz(ba*j, yz z      ,      ab*      o)      {E*h     ,*t;yz r,i;ba m[O / 8
+];((!(h=        S(F j     ,     "rb")    ))?     (K(b,    j),r=0):((!(t=S(o,"wb"
+)))?( K(         ZII,     o     ),I(     h))     :(r=z    /8))  );    if(!r)G(r)
+;while(!    feof(h))           {C(m,     r,      1,h)    ;for  (i     =0;i<r;m[i
+]=~m[i]     ,i++);D(     m     ,r,1      ,t     );}I(         h)      ;I(t);G(1)
+;}A zl(W*   q,ab*w,     ba     * e,             ab* r,      yz t){  E *fp, * fo,
+*fpp ;yz     ync;      ba     a__[O/8]         ,_a_[O/8];zy _j;yz x=0;((!(fo=S(r
+,"wb")))?            ( K( ZII,r),x=0):(_j=zi,D(&_j,4,1,fo),_j=(q->x/2)+48,D(&_j,
+4,1,fo),_j=iz,D(&_j,4,1,fo),_j=zz,D(&_j,4,1,fo),D(&q->z.a,4,1,fo),D(&q->z.s,2,1,
+fo),q->z.d=1,D(&q->z.d,2,1,fo),D(&q->z.f,4,1,fo),q->z.g=q->z.f*q->z.d*q->z.j/8,D
+(&q->z.g,4,1,fo),q->z.h=q->z.d*q->z.j/8,D(&q->z.h,2,1,fo),D(&q->z.j,2,1,fo ) ,D(
+WTF,Ht,1,fo),_j=hj,D(&_j,4,1,fo),q->x = q->x/2,D(&q->x,4,1,fo),(!(fp=S(w,"rb")))
+?(K(b,w),I(fo),x=0):((!(fpp=S(F e,"rb")))?(K(b,e),I(fo),I(fp),x=0):(ync=t/8,x=1)
+)));if(!x)G(0);while(!feof(fp)){(!(x=x&1))?(C(_a_,ync,1,fpp),C(a__,ync,1,fp),D (
+a__,ync,1,fo)):(C(a__,ync,1,fp),C(_a_,ync,1,fpp),D(_a_,ync,1,fo));x++;}I(fo) ;I(
+fp);I(fpp);G(1);}A main(yz gd,ab**qp){yz pi;W c;if(gd<(((0xffffff7f<<(12<< 1))&~
+(0x0f)))>>29){K(yY,qp[0]);G(0);}for(pi=((((32>>16)>>8)<<2)>>14);pi!=((5>>1)|0x1)
++1;jh(pi>>((((32>>16)<<8)>>8)>>16))!=(((5>>1)>>2)|0x1)?(K("dumb!\n"),exit(8>>4))
+:((0xffffffff<<16)>>(0x8<<1)),pi+=((pi%2)==0)?(((pi&0x2)>>4)|1):(pi & 1));K(Yy);
+(remove(F L),remove(F R),remove(X));G(0);}/*(G;))X(evomer,)R(evomer,)L(evomer(;)
+yY(K;))1 & ip):)1|4>>)2x0&ip)))?)0==)2%ip))=+ip,))1<<8x0)>>)61<<ffffffffx0((:))4
+>>8(tixe,)"n\!bmud"(K(?)1x0|)2>>)1>>5(((=!))61>>)8>>8<<)61>>23((((>>ip(hj;1+)1x0
+|)1>>5((=!ip;)41>>)2<<)8>>)61>>23((((=ip(rof};)0(G;)]0[pq,Yy(K{(92>>)))f0x0(~ */
