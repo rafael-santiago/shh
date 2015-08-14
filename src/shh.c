bb4f63899f8977f6c2ee750b1054f273e533e14c
rafael-santiago <voidbrainvoid@gmail.com>
2015-08-14 19:16:03 -0300

    Updating the copyright disclaimer

--- src/shh.c
+++ src/shh.c
@@ -1,5 +1,5 @@
 /*
- *                                Copyright (C) 2015 by Rafael Santiago
+ *                    Copyright (C) 2013, 2014, 2015 by Rafael Santiago
  *
  * This is a free software. You can redistribute it and/or modify under
  * the terms of the GNU General Public License version 2.
